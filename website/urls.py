61a90a8f5c4df50082cf393af9f4f7685be16bac
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-12 19:19:32 +0530

    test

--- website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from website.views import *
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-]
--- website/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from posts.models import Posts 
-
-class HomeView(ListView):
-    model = Posts
-    template_name = "home.html"    
