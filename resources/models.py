f2dc272463c353bd554d1c62395a781c8c2d718d
Ujjwal Kar <55041104+ujjwalkar0@users.noreply.github.com>
2022-08-16 22:46:53 +0530

    change

--- resources/models.py
+++ resources/models.py
@@ -4,3 +4,6 @@ from ckeditor.fields import RichTextField
 class Resources(models.Model):
     title = models.CharField(max_length=255, unique=True)
     description = RichTextField()
+
+    def __str__(self):
+        return self.title
