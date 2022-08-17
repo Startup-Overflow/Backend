c0f7c51f16e7b218b4087f54d0d5dc936c159b54
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-18 02:09:54 +0530

    ..

--- resources/models.py
+++ resources/models.py
@@ -4,6 +4,7 @@ from ckeditor.fields import RichTextField
 class Resources(models.Model):
     title = models.CharField(max_length=255, unique=True)
     description = RichTextField()
+    image = models.ImageField(max_length=255, blank=True, upload_to='resources/')
 
     def __str__(self):
         return self.title
