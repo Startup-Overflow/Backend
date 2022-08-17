059b087c1c580fbc52aa5691d07471719ddb199c
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-18 03:57:27 +0530

    ..

--- catagories/models.py
+++ catagories/models.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import User
 def superuser(): return User.objects.filter(is_superuser=True)
 
 class Catagory(models.Model):
-    username = models.ForeignKey(User, on_delete=superuser, null=True)
     name = models.CharField(max_length=50, unique=True)
-    desc = models.TextField(null=True, blank=True, default=None)
     # follower = models.ManyToManyField(User, related_name="tagfollower")
     
     def __str__(self):
