d757cfcb48cb5f23171c0d8cc37fd7bdd7063cd3
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-17 10:42:01 +0530

    catagory

--- catagories/admin.py
+++ catagories/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from catagories.models import Catagories
+from catagories.models import Catagory
 # Register your models here.
-admin.site.register(Catagories)
+admin.site.register(Catagory)
--- catagories/models.py
+++ catagories/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 def superuser(): return User.objects.filter(is_superuser=True)
 
-class Catagories(models.Model):
+class Catagory(models.Model):
     username = models.ForeignKey(User, on_delete=superuser, null=True)
     name = models.CharField(max_length=12, unique=True)
     desc = models.TextField(null=True)
