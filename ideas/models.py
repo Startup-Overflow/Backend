c71fae29dba288b4a06fbba3aa4c26e874504c77
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-18 03:53:14 +0530

    ..

--- ideas/models.py
+++ ideas/models.py
@@ -11,6 +11,6 @@ class BusinessIdea(models.Model):
     doi = models.CharField(max_length=255, unique=True) 
     url = models.CharField(max_length=255, unique=True)
     desc = RichTextField()
-    hashtag = models.ForeignKey(Hashtag)
+    hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE, null=True)
 
     def __str__(self): return self.title
