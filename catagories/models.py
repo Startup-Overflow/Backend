9456b4edacf1ce1222d1037ea8fc97ccadb792f5
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-17 10:49:23 +0530

    catagory

--- catagories/models.py
+++ catagories/models.py
@@ -6,7 +6,7 @@ def superuser(): return User.objects.filter(is_superuser=True)
 class Catagory(models.Model):
     username = models.ForeignKey(User, on_delete=superuser, null=True)
     name = models.CharField(max_length=12, unique=True)
-    desc = models.TextField(null=True, blank=True)
+    desc = models.TextField(null=True, blank=True, default=None)
     # follower = models.ManyToManyField(User, related_name="tagfollower")
     
     def __str__(self):
