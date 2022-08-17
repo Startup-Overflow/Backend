83927b8cb3419549ac730c16b8687683854b3a65
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-17 23:44:47 +0530

    11

--- resources/urls.py
+++ resources/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from resources.views import *
 urlpatterns = [
     path('', ResourcesHandler.as_view(), name='resources'),
-    path('<pk:int>', ResourcesHandler.as_view(), name='resourcesid'),
+    path('<int:pk>', ResourcesHandler.as_view(), name='resourcesid'),
 ]
