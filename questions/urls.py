a9e078b62565491421400f1c690a510e6d6e6261
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-08-20 03:56:48 +0530

    ..

--- questions/urls.py
+++ questions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from questions.views import QuestionsView
 
 urlpatterns = [
-    path('',QuestionsView.as_view())
+    path('',QuestionsView.as_view()),
+    path('<int:pk>/',QuestionsView.as_view())
 ]
